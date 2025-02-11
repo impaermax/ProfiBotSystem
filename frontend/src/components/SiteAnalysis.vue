c06ab61c601b67a41a6e862574a7854b3f6a0f97
impaermax <mcreativeprint@gmail.com>
2025-02-11 21:38:44 +0300

    Create SiteAnalysis.vue

---
+++ frontend/src/components/SiteAnalysis.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Анализ сайта</h2>
+    <input v-model="url" placeholder="Введите URL" />
+    <button @click="analyze">Анализировать</button>
+    <pre>{{ result }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      url: "",
+      result: {},
+    };
+  },
+  methods: {
+    async analyze() {
+      const response = await fetch("/api/site-analysis", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ url: this.url }),
+      });
+      this.result = await response.json();
+    },
+  },
+};
+</script>
