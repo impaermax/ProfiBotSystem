de2fad38ce92f2dbf6eab2d0e6664583e4fa7ac6
impaermax <mcreativeprint@gmail.com>
2025-02-11 21:36:52 +0300

    Create App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+body {
+  background-color: white;
+  color: #6a0dad;
+  font-family: Arial, sans-serif;
+}
+</style>
